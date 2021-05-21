5dc66e12f7bc4ac4f5b1b05c66661e2b19287fa3
namdev1994 <n.lavhare1994@gmail.com>
2021-05-22 00:32:24 +0530

    Added: Check cheapiest Hotel

--- src/main/java/hotelreservationsystem/Hotel.java
+++ src/main/java/hotelreservationsystem/Hotel.java
@@ -4,8 +4,27 @@ public class Hotel {
     String hotelName;
     int customerRate;
 
-    public Hotel(String hotelName,int customerRate) {
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getCustomerRate() {
+        return customerRate;
+    }
+
+    public void setCustomerRate(int customerRate) {
+        this.customerRate = customerRate;
+    }
+
+    public Hotel(String hotelName, int customerRate) {
         this.hotelName=hotelName;
         this.customerRate=customerRate;
+    }
+    public void setRate(int noOfDays) {
+        this.customerRate = this.customerRate*noOfDays;
     }
 }
--- src/main/java/hotelreservationsystem/HotelReservation.java
+++ src/main/java/hotelreservationsystem/HotelReservation.java
@@ -1,7 +1,13 @@
 package hotelreservationsystem;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
     static List<Hotel> hotelsList=new ArrayList<Hotel>();
@@ -13,7 +19,43 @@ public class HotelReservation {
     public List<Hotel> getHotelsList() {
         return hotelsList;
     }
+
+    public int NoOfDays(String firstDate,String lastDate) {
+
+
+            // Create an instance of the SimpleDateFormat class
+            SimpleDateFormat obj = new SimpleDateFormat("MM-dd-yyyy");
+
+            // In the try block, we will try to find the difference
+            try {
+
+                // Use parse method to get date object of both dates
+                Date date1 = obj.parse(firstDate);
+                Date date2 = obj.parse(lastDate);
+
+                // Calucalte time difference in milliseconds
+                int time_difference = (int) (date2.getTime() - date1.getTime());
+
+                // Calucalte time difference in days using TimeUnit class
+                int days_difference = (int) (TimeUnit.MILLISECONDS.toDays(time_difference) % 365);
+            }
+            // Catch parse exception
+            catch (ParseException excep) {
+                excep.printStackTrace();
+            }
+        int days_difference = 0;
+        return days_difference;
+        }
+
+
+    public Hotel checkChepiesRatetHotel(int NoOfDays) {
+        hotelsList.stream().map(p -> {p.setRate(NoOfDays); return p.customerRate; }).collect(Collectors.toList());
+        Hotel cheapestRate =  hotelsList.stream().min(Comparator.comparing(Hotel::getCustomerRate)).orElse(null);
+        return cheapestRate;
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome In Hotel Reservation System");
     }
+
 }
